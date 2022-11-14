86003c930c2c4ff20ba69bd7d8044fd255d35131
Jack Stephens <jack.stephens@me.com>
2022-11-14 12:36:44 +0000

    strange body css problem so wrapped in container

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,18 @@
+class QuestionsController < ApplicationController
+
+  def ask
+  end
+
+  def answer
+    @question = params.values[0]
+
+    @answer = if @question.downcase == "i am going to work right now!"
+                "Great!"
+              elsif @question.end_with?("?")
+                "Silly question, get dressed and go to work!"
+              else
+                "I don't care, get dressed and go to work!"
+              end
+
+  end
+end
